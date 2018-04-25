e9ef1bd83875ec173328e1c3cc45ce6748a8723f
smandekar1 <smandekar1@yahoo.com>
2018-04-25 14:47:33 -0500

    heroku

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for, render_template
 from config import Config
 
-#s
+#ss
 app = Flask(__name__)
 
 app.debug = True
